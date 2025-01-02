2109672c581ad085c0c6560ad4620d3637df6eaf
Keerthana-Singaravel <143105458+Keerthana-Singaravel@users.noreply.github.com>
2025-01-02 21:40:34 +0530

    TestAccess.java

--- day5/Access_Specifiers/TestPackage/TestAccess.java
+++ day5/Access_Specifiers/TestPackage/TestAccess.java
@@ -1,9 +1,10 @@
 package TestPackage;
-
+import MainPackage.Person;
 public class TestAccess {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Person p=new Person("Keerthu",21,"ABC Salem",67000);
+		System.out.println("Name: "+p.name);
+		System.out.println("Salary: "+p.Salary());
 
 	}
 
