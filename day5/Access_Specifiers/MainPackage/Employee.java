8436e2e277da423ac1f3159363d651f1f0fd28fb
Keerthana-Singaravel <143105458+Keerthana-Singaravel@users.noreply.github.com>
2025-01-02 21:39:25 +0530

    Employee.java

--- day5/Access_Specifiers/MainPackage/Employee.java
+++ day5/Access_Specifiers/MainPackage/Employee.java
@@ -1,5 +1,13 @@
 package MainPackage;
 
 public class Employee {
-
+public Employee(String name, int age, String address, double salary) {
+		super(name, age, address, salary);
+	}
+    public void Display() {
+    	System.out.println("Name "+name);
+    	System.out.println("Age :"+age);
+    	System.out.println("Address:"+address);
+    	System.out.println("Salary :"+Salary());
+    }
 }
